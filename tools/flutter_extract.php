f01523997b570cc9e20a2bbfb888cdcdad13052a
Alan Knowles <alan@roojs.com>
2019-03-27 19:08:41 +0800

    tools/flutter_extract.php

--- tools/flutter_extract.php
+++ tools/flutter_extract.php
@@ -444,7 +444,10 @@ foreach(eClass::$all as $c) {
     $c->readDocs();
     $summary[$c->name] = $c->toSummaryArray();
     file_put_contents(FDIR .'json/index.json', json_encode($summary, JSON_PRETTY_PRINT));
-    file_put_contents(FDIR .'json/'.$c->name. '.json', json_encode($c,JSON_PRETTY_PRINT));
+    if (is_a($c, 'eClass')) {
+        file_put_contents(FDIR .'json/'.$c->name. '.json', json_encode($c,JSON_PRETTY_PRINT));
+    }
+    // constant's and other stufff. - 
 }
 
 print_r(eClass::$all);
