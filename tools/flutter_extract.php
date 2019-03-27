f85531b4e77c4b64bad620e62b1020ad2c59ec2b
Alan Knowles <alan@roojs.com>
2019-03-27 18:11:41 +0800

    tools/flutter_extract.php

--- tools/flutter_extract.php
+++ tools/flutter_extract.php
@@ -55,6 +55,11 @@ class Obj {
     }
     function parseType($sp)
     {
+        if (!$sp) {
+            print_R($this);
+            echo "parseType got invalid value";
+            exit;
+        }
         $ar = $sp->getElementsByTagName('a');
         if ($ar->length == 1) {
             $this->type = eClass::$url_map[$ar->item(0)->getAttribute('href')]->name;
