df1a31e9d24e1649b2e169ce58d7f95cd43b1bc4
Alan Knowles <alan@roojs.com>
2019-03-28 15:42:48 +0800

    tools/flutter_extract.php

--- tools/flutter_extract.php
+++ tools/flutter_extract.php
@@ -178,7 +178,7 @@ class eClass extends Obj {
     static $all = array();
     static $url_map = array();
     var $name;
-    var $extends;
+    var $extends = false;
     var $memberOf; /// really the package..
     var $events = array();
     var $methods = array();
