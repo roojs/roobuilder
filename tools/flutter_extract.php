b152d49c74f41d5685fc9d2a3808e134728cf263
Alan Knowles <alan@roojs.com>
2019-03-28 18:01:35 +0800

    tools/flutter_extract.php

--- tools/flutter_extract.php
+++ tools/flutter_extract.php
@@ -181,54 +181,56 @@ class Ns extends Obj {
     {
         self::$kv[$cls->memberOf]->cn[] = $cls;
     }
+    
+    function fakeTree()
+    {
+         
+        foreach($this->cn as $c) {
+            if (!isset($c->shortname))  {
+                continue;
+            }
+            $map[$c->shortname] = $c;
+            
+           
+              
+            
+        }
+        
+        $cn = array();
+        foreach($this->cn as $c) {
+            if (!isset($c->shortname))  {
+                continue;
+            }
+            $bits = preg_split('/(?<=[a-z])(?=[A-Z])|(?=[A-Z][a-z])/',
+                                 $c->shortname, -1, PREG_SPLIT_NO_EMPTY);
+            //print_r($bits);
+            if (count($bits) < 2 ) {
+                continue;   
+            }
+            if (!isset($map[$bits[0]])) {
+                $add = new Ns(array(
+                    'name' => $c->memberOf .'.'. $bits[0],
+                    'isFakeNamespace' => true,
+                    
+                ));
+                $map[$bits[0]] = $add;
+                $cn[] = $add;
+            }
+            
+            $map[$bits[0]]->cn[] = $c;
+            
+            
+        }
+        $this->cn = $cn;
+        
+ 
+    }
+    
     function toTreeArray()
     {
         // tidy the tree before starting...
         
-        if (false) {
-            
-            foreach($this->cn as $c) {
-                if (!isset($c->shortname))  {
-                    continue;
-                }
-                $map[$c->shortname] = $c;
-                
-               
-                  
-                
-            }
-            
-            $cn = array();
-            foreach($this->cn as $c) {
-                if (!isset($c->shortname))  {
-                    continue;
-                }
-                $bits = preg_split('/(?<=[a-z])(?=[A-Z])|(?=[A-Z][a-z])/',
-                                     $c->shortname, -1, PREG_SPLIT_NO_EMPTY);
-                //print_r($bits);
-                if (count($bits) < 2 ) {
-                    continue;   
-                }
-                if (!isset($map[$bits[0]])) {
-                    $add = new Ns(array(
-                        'name' => $c->memberOf .'.'. $bits[0],
-                        'isFakeNamespace' => true,
-                        
-                    ));
-                    $map[$bits[0]] = $add;
-                    $cn[] = $add;
-                }
-                
-                $map[$bits[0]]->cn[] = $c;
-                
-                
-            }
-            $this->cn = $cn;
-            
-            if ($cn) {
-                print_R($cn);
-            }
-        }
+        
         
         $ret = array(
             'name' => $this->name,
