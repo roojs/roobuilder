92086f07680f91e2c04f9841cf7250ad19b00623
Alan Knowles <alan@roojs.com>
2019-04-16 17:46:34 +0800

    tools/flutter_sqlite.php

--- tools/flutter_sqlite.php
+++ tools/flutter_sqlite.php
@@ -180,7 +180,7 @@ class fsql {
         $this->extends = array();        
         for($i = $as->length-1;$i > -1; $i--) {
 
-            $ex = $this->lookup('href', $as->item($i)->getAttribute('href'))
+            $ex = $this->lookup('href', $as->item($i)->getAttribute('href'));
             if ($ex) {
                 die("could not find " . $as->item($i)->getAttribute('href') . " when parsing" . $id);
             }
