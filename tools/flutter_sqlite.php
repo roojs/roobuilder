572b9ca7986068f41f2db58ccf6a5b4c7e68cd06
Alan Knowles <alan@roojs.com>
2019-04-16 17:51:48 +0800

    tools/flutter_sqlite.php

--- tools/flutter_sqlite.php
+++ tools/flutter_sqlite.php
@@ -65,6 +65,7 @@ class fsql {
         if (empty($o)) {
             return;
         }
+        print_r($o);
         foreach((array) $o as $k=>$v) {
             if (is_a($v,'stdClass')) {
                 foreach((array)$v as $ik  => $iv) {
