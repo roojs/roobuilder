20bf823f013ae9f07e59ef51dde02b07e614368f
Alan Knowles <alan@roojs.com>
2019-04-17 16:56:19 +0800

    tools/flutter_sqlite.php

--- tools/flutter_sqlite.php
+++ tools/flutter_sqlite.php
@@ -7,7 +7,7 @@
  -- contructor with generic eg. const AlwaysStoppedAnimation<T>(
  -- classes - we need to parse the right hand column to determine which properties/ methods  are static..
  -- enums have methods !! 
- -- types on topLevelconstants are broken
+ -- types on topLevelconstants are broken (might be able to extract it from the implementation.)
  */
 
 class fsql {
