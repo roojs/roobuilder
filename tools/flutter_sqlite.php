b303502e5b77136c57a5caae6ac149dfc7235426
Alan Knowles <alan@roojs.com>
2019-04-17 16:40:33 +0800

    tools/flutter_sqlite.php

--- tools/flutter_sqlite.php
+++ tools/flutter_sqlite.php
@@ -452,7 +452,8 @@ class fsql {
             var_dump($cn->nodeName);
             switch($cn->nodeName) {
                 case 'dt': // look for name
-                    $name = $this->getElementsByClassName($ct->ownerDocument,'name',$cn)->item(0)->textContents;
+                    $name = $this->getElementsByClassName($ct->ownerDocument,'name',$cn)->item(0)->textContent;
+                    
                     $n = array(
                         'type' => 'enum-value',
                         'parent_id' => $o['id'],
