bb2108423f36913d9811808a1267a73dff24751c
Alan Knowles <alan@roojs.com>
2025-01-31 22:45:18 +0800

    resources/doc/gtk.html resources/doc/roojs-bootstrap-debug.js resources/doc/roojs-core-debug.js resources/doc/Roo.docs.js resources/doc/Roo.docs.init.js resources/doc/Roo.docs.template.js resources/doc/css-bootstrap4/bootstrap.min.css resources/doc/css-bootstrap4/roojs-bootstrap.css resources/doc/css-bootstrap/font-awesome.css resources/doc/css-bootstrap/font-awesome/fa-brands-400.woff2 resources/doc/css-bootstrap/font-awesome/fa-regular-400.woff2 resources/doc/css-bootstrap/font-awesome/fa-solid-900.woff2 resources/doc/docs.css

--- resources/doc/Roo.docs.init.js
+++ resources/doc/Roo.docs.init.js
@@ -539,7 +539,10 @@ Roo.docs.init = {
         inherits.forEach(function(sc) {
             
             var cc = this.classes[sc];
-            Roo.log(JSON.stringify(cc, null,2));
+            if (typeof(cc) == 'undefined') {
+                Roo.log("Missing? " + sc);
+                return;
+            }
             if (cc.stype == this.SymbolKind.Class) {
                 orig.augments.push(sc);
             } else if (orig.implements.indexOf(sc) < 0) {
